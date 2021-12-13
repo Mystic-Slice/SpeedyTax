d4b88e9aa35c550f4fec0fc12192a17ff930bcd4
Ashwath V A <vaashwath@gmail.com>
2021-12-13 22:28:01 +0530

    Boilerplate

---
+++ src/Components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export function App() {
+    return <div>
+        <h1>Speedy Tax</h1>
+        </div>
+}
---
+++ src/renderer.ts
@@ -0,0 +1,5 @@
+import './index.css';
+import * as ReactDOM from 'react-dom';
+import { App } from './Components/App';
+
+ReactDOM.render(App(), document.getElementById('root'));
