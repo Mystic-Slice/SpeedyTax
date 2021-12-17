b23de0a24d7ec6029162969b54ef0951d74c0064
Ashwath V A <vaashwath@gmail.com>
2021-12-17 17:10:53 +0530

    Signin Page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,9 +3,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
+import { Login } from './Pages/Login';
 import TaxFilingPage from './Pages/TaxFilingPage';
 
+export interface User {
+    userName: string,
+    password: string,
+    type: "client" | "consultant"
+}
+
 export function App() {
+    const [user, setUser] = React.useState(null as User);
+
+    if(!user) {
+        return <Login setUser={setUser}/>
+    }
+    
     return (
         <HashRouter>
             <Routes>
